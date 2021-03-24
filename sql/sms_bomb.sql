19dbb7eebccb85643e53355d71d77e633dc6bedd
leijinjun <986111634@qq.com>
2021-03-24 21:42:27 +0800

    SQL脚本

---
+++ sql/sms_bomb.sql
@@ -0,0 +1,53 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for s_sms_send_log
+-- ----------------------------
+DROP TABLE IF EXISTS `s_sms_send_log`;
+CREATE TABLE `s_sms_send_log`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `request_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `ip` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `sms_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `params` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `response` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_at` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `request_duration` int(11) NULL DEFAULT NULL COMMENT '请求持续时间单位ms',
+  `response_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'SUCCESS、FAILURE',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_createAt`(`create_at`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for s_sms_url
+-- ----------------------------
+DROP TABLE IF EXISTS `s_sms_url`;
+CREATE TABLE `s_sms_url`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '网站logo',
+  `website_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '网站名称',
+  `website` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '网站首页',
+  `sms_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '短信接口',
+  `phone_param_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号名称',
+  `binding_params` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '其它固定请求参数，json字符串',
+  `create_at` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `update_at` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
+  `is_normal` tinyint(1) NULL DEFAULT 1 COMMENT '1=正常，0=失效',
+  `business_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '业务类型（标注用）。如：注册、找回',
+  `success_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '响应成功code。key=code,value=200',
+  `end_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `open_script` tinyint(1) NULL DEFAULT NULL COMMENT '是否使用脚本，1=标识使用',
+  `script_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '脚本名称',
+  `script_content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '脚本内容',
+  `script_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '脚本执行路径。优先使用字段script_content',
+  `request_method` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'GET、POST',
+  `content_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '请求参数类型：application/x-www-form-urlencoded、application/json',
+  `headers` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '固定请求头。例：[Refer=xxx][key=value]',
+  `response_type` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'json' COMMENT '响应内容类型。如：json、xml、text',
+  `last_used_time` datetime(0) NULL DEFAULT NULL COMMENT '最后使用时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
